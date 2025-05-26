4d153f6edc25be94160da9e7c1b5ade7fdd51dd3
Christian Djoko <djokodev75@gmail.com>
2025-05-26 04:09:07 +0100

    Add user registration functionality: create UserRegistrationForm, implement registration view, and update URL routing. Enhance login template with registration link.

--- account/views.py
+++ account/views.py
@@ -3,12 +3,34 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+
+            password_clair = []
+            password_clair.append(user_form.cleaned_data['password'])
+
+            print(password_clair)
+
+            # hach password before save in DB
+            new_user.set_password(user_form.cleaned_data['password'])
+
+            new_user.save()
+
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+        
+    else:
+        user_form = UserRegistrationForm()
+        return render(request, 'account/register.html', {'user_form': user_form})
 
 def user_login(request):
     """User login view"""
